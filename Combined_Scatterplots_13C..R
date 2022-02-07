d55f0d1456bd3ca3e469a2d34ef0637cdb17565e
Fabio-Trevisan <98732667+Fabio-Trevisan@users.noreply.github.com>
2022-02-07 17:35:33 +0100

    Scatterplot script for combination of multiple scatter-lines with error bars
    a) starting from averages and std.errors (DONE)
    b) starting from raw data matrix (TO DO)

---
+++ Combined_Scatterplots_13C..R
@@ -0,0 +1,33 @@
+library(readr)
+library(ggpubr)
+library(ggplot2)
+library(reshape2)
+library(tidyverse)
+library(plyr)
+library(dplyr)
+
+
+
+table <- read.csv("13C_IRMS_Summary_R_data.csv", sep=";",
+                  header=T)
+
+
+#Scatter line + error bars (STD.Err) --> used means and errors from excel NOT calculated in R 
+f1 <- ggplot(table, aes(x = Time, y = Mean, group = Treatment, colour = Treatment)) + 
+  geom_line(aes(group = Treatment)) + 
+  geom_point(aes(shape = Treatment)) + scale_shape_manual(values = c(15:18)) +
+  geom_errorbar(aes(ymin = Mean-S.Err, ymax = Mean+S.Err, group = Treatment), width = 0.5) +
+  theme_bw() + 
+  scale_y_continuous(breaks=seq(-40,-11,1)) +
+  scale_x_continuous(breaks=seq(0,16,1))
+f1 + facet_wrap(~Species_Tissue, scales="free", ncol = 2) + 
+  ylab("13C") + 
+  xlab("Time (Days)") + 
+  scale_fill_manual(values=c("grey77", "skyblue3", "darkorange2", "slateblue3"))
+
+ggsave(filename = "13C_Scatter-lines_errorbars(bw).pdf", plot = last_plot(), dpi = 600, units = "cm", width = 60, height = 60, scale = 0.5)
+
+
+
+#TO DO
+#Scatter line + error bars (STD.Err) --> calculate means and errors in R 
