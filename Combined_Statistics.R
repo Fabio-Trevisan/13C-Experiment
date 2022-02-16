04e58bd8f126a4da3c98a00a642c99535276a08a
Fabio-Trevisan <98732667+Fabio-Trevisan@users.noreply.github.com>
2022-02-16 14:16:42 +0100

    OneWay Anova generation works (TO OPTIMIZE)
    Title integration (ARTofR package)
    Tukey works (o automatize generation)

--- Combined_Statistics.R
+++ Combined_Statistics.R
@@ -5,8 +5,16 @@ library(plyr)
 library(dplyr)
 library(rstatix)
 library(purrr)
+library(agricolae)
+library(ARTofR)
 
-#calculate means and errors in R 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                                                            --
+##BASIC_STATISTICS------
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#calculate means and errors in R----
+
 table <- read.csv("13C_IRMS_R_data.csv", sep=";",
                    header=T)
 
@@ -22,7 +30,7 @@ table <- read.csv("13C_IRMS_R_data.csv", sep=";",
 #compare_means(Value ~ Treatment, group.by =  c("Time","Species_Tissue"), data = table, method = "t.test") 
 
 
-#creare Subsets according to Species_Tissue
+#creare Subsets according to Species_Tissue----
 Tomato_Root <- subset(table, Species_Tissue == "Tomato_Root")
 head(Tomato_Root)
 Tomato_Shoot <- subset(table, Species_Tissue == "Tomato_Shoot")
@@ -40,11 +48,13 @@ head(Barley_Root)
 Barley_Shoot <- subset(table, Species_Tissue == "Barley_Shoot")
 head(Barley_Shoot)
 
-#transform variable to factor
+
+
+#transform variable to factor----
 table$Time <- factor(table$Time)
 
 
-#Shapiro-Wilk normality check and/or qq-plot and/or density plot
+#Shapiro-Wilk normality check and/or qq-plot and/or density plot----
 #for all single treatments
 SW_test <- table %>%
   group_by(Time, Treatment, Species_Tissue) %>%
@@ -53,38 +63,116 @@ View(SW_test)
 write.table(SW_test, file = "ShapiroWilk_test_results.csv", quote = FALSE, sep = ";")
 
 #non serve 
-#density_plot <- ggdensity(Species_Tissue$Value, main = "Density plot 13C", xlab = "Delta 13C")
-#density_plot
+density_plot <- ggdensity(Tomato_Root$Value, main = "Density plot 13C", xlab = "Delta 13C")
+density_plot
 
-#non serve 
-#qq_plot <- ggqqplot(Species_Tissue$Value)
-#qq_plot
-
-
-
-#1way ANOVA (NOT WORKING)
-OneWay_Anova_Treatment <- lapply(split(table, list(table$Species_Tissue,table$Time)), 
-                                 anova(lm(Value ~ Treatment, data = table)))
+#non serve
+qq_plot <- ggqqplot(Tomato_Root$Value)
+qq_plot
 
 
-#2way ANOVA
 
+#1way ANOVA (TO OPTIMIZE)----
+##Treatment
+OneWay_Anova_Tr_tr <- lapply(split(Tomato_Root, Tomato_Root$Time), function(i){ 
+                                 anova(lm(Value ~ Treatment, data = i))
+})
+write.table(OneWay_Anova_Tr_tr, file = "OneWay_Anova_Tr_tr.csv", quote = FALSE, sep = ";")
+
+OneWay_Anova_Tr_ts <- lapply(split(Tomato_Shoot, Tomato_Shoot$Time), function(i){ 
+  anova(lm(Value ~ Treatment, data = i))
+})
+
+OneWay_Anova_Tr_br <- lapply(split(Barley_Root, Barley_Root$Time), function(i){ 
+  anova(lm(Value ~ Treatment, data = i))
+})
+
+OneWay_Anova_Tr_bs <- lapply(split(Barley_Shoot, Barley_Shoot$Time), function(i){ 
+  anova(lm(Value ~ Treatment, data = i))
+})
+
+OneWay_Anova_Tr_mr <- lapply(split(Maize_Root, Maize_Root$Time), function(i){ 
+  anova(lm(Value ~ Treatment, data = i))
+})
+
+OneWay_Anova_Tr_ms <- lapply(split(Maize_Shoot, Maize_Shoot$Time), function(i){ 
+  anova(lm(Value ~ Treatment, data = i))
+})
+
+OneWay_Anova_Tr_cr <- lapply(split(Cucumber_Root, Cucumber_Root$Time), function(i){ 
+  anova(lm(Value ~ Treatment, data = i))
+})
+
+OneWay_Anova_Tr_cs <- lapply(split(Cucumber_Shoot, Cucumber_Shoot$Time), function(i){ 
+  anova(lm(Value ~ Treatment, data = i))
+})
+
+##Time
+OneWay_Anova_Ti_tr <- lapply(split(Tomato_Root, Tomato_Root$Treatment), function(i){ 
+  anova(lm(Value ~ Time, data = i))
+})
+
+OneWay_Anova_Ti_ts <- lapply(split(Tomato_Shoot, Tomato_Shoot$Treatment), function(i){ 
+  anova(lm(Value ~ Time, data = i))
+})
+
+OneWay_Anova_Ti_br <- lapply(split(Barley_Root, Barley_Root$Treatment), function(i){ 
+  anova(lm(Value ~ Time, data = i))
+})
+
+OneWay_Anova_Ti_bs <- lapply(split(Barley_Shoot, Barley_Shoot$Treatment), function(i){ 
+  anova(lm(Value ~ Time, data = i))
+})
+
+OneWay_Anova_Ti_mr <- lapply(split(Maize_Root, Maize_Root$Treatment), function(i){ 
+  anova(lm(Value ~ Time, data = i))
+})
+
+OneWay_Anova_Ti_ms <- lapply(split(Maize_Shoot, Maize_Shoot$Treatment), function(i){ 
+  anova(lm(Value ~ Time, data = i))
+})
+
+OneWay_Anova_Ti_cr <- lapply(split(Cucumber_Root, Cucumber_Root$Treatment), function(i){ 
+  anova(lm(Value ~ Time, data = i))
+})
+
+OneWay_Anova_Ti_cs <- lapply(split(Cucumber_Shoot, Cucumber_Shoot$Treatment), function(i){ 
+  anova(lm(Value ~ Time, data = i))
+})
+
+##test
+OneWay_Anova_xx <- aov(Value ~ Treatment, data = Tomato_Root)
+summary(OneWay_Anova_xx)
+
+
+#2way ANOVA----
 ##Single Species_Tissue
 Tomato_Root$Time <- factor(Tomato_Root$Time)
 TwoWay_Anova_tr <- aov(Value ~ Treatment * Time, data = Tomato_Root)
+summary(TwoWay_Anova_tr)
+...*8
+
 
 ##Multiple Species_Tissue
 TwoWay_Anova <- lapply(split(table, table$Species_Tissue), function(i){
   anova(lm(Value ~ Treatment * Time, data = i))
 })
-summary(TwoWay_Anova)
+View(TwoWay_Anova[["Species_Tissue"]])
+
+write.table(TwoWay_Anova, file = "TwoWay_Anova_results.csv", quote = FALSE, sep = ";")
 
 
-# Tukey as post hoc test
-#work only on sigle anovas not on lapply list --> maybe try sapply/tapply
-TukeyHSD(TwoWay_Anova_tr)
+# Tukey as post hoc test----
+#work only on sigle anovas not on lapply list
+TukeyHSD(OneWay_Anova_Tr_tr)
+
+Tukey_test <- HSD.test(OneWay_Anova_xx, "Treatment")
+Tukey_test2 <- HSD.test(OneWay_Anova_Tr_tr["0"], "Treatment")
 
 
+
+
+#Assumptions----
 # 1. Homogeneity of variances
 plot(TwoWay_Anova_tr, 1)
 levene_test(Value ~ Treatment * Time, data = Tomato_Root)
@@ -95,15 +183,13 @@ plot(TwoWay_Anova_tr, 2)
 ## Extract the residuals
 aov_residuals <- residuals(object = TwoWay_Anova_tr)
 ## Run Shapiro-Wilk test
-shapiro.test(x = aov_residuals )
+shapiro.test(x = aov_residuals)
+shapiro.test(Tomato_Root)
 
 
 
-
-
-#Kruskal Wallis NOT WORKING
+#Kruskal Wallis NOT WORKING ----
 ##Single Species_Tissue
-Tomato_Root$Time <- factor(Tomato_Root$Time)
 TwoWay_KW_tr <- kruskal.test(Value ~ Treatment * Time, data = Tomato_Root)
 
 ##Multiple Species_Tissue
@@ -116,3 +202,4 @@ kruskal.test(Value ~ Treatment, data = table)
 #or
 kruskal_test(table, Value ~ Treatment)
 
+dunn test
